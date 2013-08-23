547e32fcde3abcbc1ff14b2a0250089b37a2ed6f
Daniel Pakuschewski <contato@danielpk.com.br>
2013-08-23 13:19:25 -0300

    Droped nginx support

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,11 +11,6 @@ class apache::config {
   $logdir     = "${boxen::config::logdir}/apache"
   $logerror   = "${logdir}/error.log"
 
-  # Nginx Proxy log files
-  $proxyaccesslogfile = "${logdir}/nginx-access.log"
-  $proxyerrorlogfile  = "${logdir}/nginx-error.log"
-
-  # Our port! As nginx takes 80, lets bump by 10000
-  $port = '10080'
-
+  $port = '80'
+  
 }
